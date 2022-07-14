3df6d223994535cc15b18b2297ed5dda0c0f8676
lhk3337 <lhk3337@naver.com>
2022-07-14 17:58:44 +0900

    [FEAT] koa Middleware

--- src/index.js
+++ src/index.js
@@ -2,8 +2,24 @@ const Koa = require('koa');
 
 const app = new Koa();
 
+app.use(async (ctx, next) => {
+  console.log(ctx.url);
+  console.log(1);
+  if (ctx.query.authorized !== '1') {
+    ctx.status = 401;
+    return;
+  }
+  await next();
+  console.log('End');
+});
+
+app.use((ctx, next) => {
+  console.log(2);
+  next();
+});
+
 app.use((ctx) => {
-  ctx.body = 'hello';
+  ctx.body = 'hello world';
 });
 
 app.listen(4000, () => {
