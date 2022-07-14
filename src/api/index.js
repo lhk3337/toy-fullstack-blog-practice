ab9909202090c080d6bc0f549f340a95483732c9
lhk3337 <lhk3337@naver.com>
2022-07-14 18:21:08 +0900

    [FEAT] posts module

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,6 @@
 const Router = require('koa-router');
+const posts = require('./posts');
 const api = new Router();
 
-api.get('/test', (ctx) => {
-  ctx.body = 'test 입니다.';
-});
-
+api.use('/posts', posts.routes());
 module.exports = api;
