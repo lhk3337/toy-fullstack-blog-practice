aaef79e787a8d4a9cb03587de97fef6b67ae069e
lhk3337 <lhk3337@naver.com>
2022-07-18 18:25:38 +0900

    [FEAT] Login 구현

--- src/api/auth/auth.ctrl.js
+++ src/api/auth/auth.ctrl.js
@@ -34,6 +34,27 @@ export const register = async (ctx) => {
     ctx.throw(500, e);
   }
 };
-export const login = (ctx) => {};
+export const login = async (ctx) => {
+  const { username, password } = ctx.request.body;
+  if (!username || !password) {
+    ctx.status = 401;
+    return;
+  }
+  try {
+    const user = await User.findByUsername(username);
+    if (!user) {
+      ctx.status = 401;
+      return;
+    }
+    const valid = await user.checkPassword(password);
+    if (!valid) {
+      ctx.status = 401;
+      return;
+    }
+    ctx.body = user.serialize();
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
 export const check = (ctx) => {};
 export const logout = (ctx) => {};
