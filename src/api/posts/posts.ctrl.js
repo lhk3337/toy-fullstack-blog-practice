1c2d65b4fa0c4a57f55aa8329e68ccef6c2a9574
lhk3337 <lhk3337@naver.com>
2022-07-19 22:46:36 +0900

    [FEAT] 회원 인증 설정하기

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -4,13 +4,24 @@ import Joi from '@hapi/joi';
 
 const { ObjectId } = mongoose.Types;
 
-export const checkObjectId = (ctx, next) => {
+export const getPostByid = async (ctx, next) => {
   const { id } = ctx.params;
   if (!ObjectId.isValid(id)) {
     ctx.status = 400;
     return;
   }
-  return next();
+  try {
+    const post = await Post.findById(id);
+    if (!post) {
+      ctx.status = 404;
+      return;
+    }
+
+    ctx.state.post = post;
+    return next();
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
 
 export const write = async (ctx) => {
@@ -31,6 +42,7 @@ export const write = async (ctx) => {
     title,
     body,
     tags,
+    user: ctx.state.user,
   });
 
   try {
@@ -47,14 +59,19 @@ export const list = async (ctx) => {
     ctx.status = 400;
     return;
   }
+  const { tag, username } = ctx.query;
+  const query = {
+    ...(username ? { 'user.username': username } : {}),
+    ...(tag ? { targets: tag } : {}),
+  };
 
   try {
-    const posts = await Post.find()
+    const posts = await Post.find(query)
       .sort({ _id: -1 }) // 내림차순 정렬 sort(),
       .limit(10) // 개수 제한 limit(갯수)
       .skip((page - 1) * 10) //page 1일때 skip(0), page 2일때 skip(10): 데이터 10까지 생략하고 11부터 출력
       .exec();
-    const postCount = await Post.countDocuments().exec();
+    const postCount = await Post.countDocuments(query).exec();
     ctx.set('Last-Page', Math.ceil(postCount / 10)); // 마지막 페이지 출력
     ctx.body = posts
       .map((post) => post.toJSON())
@@ -75,10 +92,19 @@ export const read = async (ctx) => {
       ctx.status = 404;
       return;
     }
-    ctx.body = post;
+    ctx.body = ctx.state.post;
   } catch (e) {
     ctx.throw(500, e);
   }
+};
+
+export const checkOwnPost = (ctx, next) => {
+  const { user, post } = ctx.state;
+  if (post.user._id.toString() !== user._id) {
+    ctx.status = 403;
+    return;
+  }
+  return next();
 };
 
 export const remove = async (ctx) => {
