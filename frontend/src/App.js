5289eb889a317f1df3c838fc0e780c9a87cb34e4
lhk3337 <lhk3337@naver.com>
2022-07-20 20:42:41 +0900

    [FEAT] styled-component 설치 및 globalStyle 설정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 import Routers from "components/Router";
+import GlobalStyles from "components/GlobalStyles";
 function App() {
   return (
     <div className="App">
+      <GlobalStyles />
       <Routers />
     </div>
   );
---
+++ frontend/src/components/GlobalStyles.js
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset"; // style-reset 패키지
+
+const GlobalStyles = createGlobalStyle`
+    ${reset}
+    a{
+        text-decoration: none;
+        color: inherit;
+    }
+    *{
+        box-sizing: border-box;
+    }
+    #root {
+        min-height: 100%;
+    }
+    html {
+        height: 100%;
+    }
+    body {
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        font-size: 14px;
+        box-sizing: border-box;
+        min-height: 100%;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+    code {
+        font-family: source-code-pro, Menlo, Monaco, Consolas, ‘Courier New‘, monospace;
+    }
+`;
+
+export default GlobalStyles;
