7a3f9e1b7187ac2efec1e51aa3d0d4750913fe3d
lhk3337 <lhk3337@naver.com>
2022-07-21 23:37:12 +0900

    [FEAT] mongo db connect

---
+++ backend/src/models/index.js
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import mongoose from 'mongoose';
+const { MONGO_URI } = process.env;
+
+const connect = () => {
+  mongoose.connect(MONGO_URI, (error) => {
+    if (error) {
+      console.log('MongoDB error', error);
+    } else {
+      console.log('Connected MongoDB');
+    }
+  });
+};
+mongoose.connection.on('error', (error) => {
+  console.error('mongoDB error', error);
+});
+mongoose.connection.on('disconnected', () => {
+  console.error('mongoDB disconnection reply connect');
+  connect();
+});
+
+export default connect;
