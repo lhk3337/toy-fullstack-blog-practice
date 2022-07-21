a2b08c9815e829d4d03387fd250b3e4d6204c51d
lhk3337 <lhk3337@naver.com>
2022-07-22 00:49:07 +0900

    [FEAT] Auth UI에서 login UI register UI 분리

--- frontend/src/components/auth/AuthForm.js
+++ frontend/src/components/auth/AuthForm.js
@@ -46,20 +46,27 @@ const ButtonWidthMarginTop = styled(Button)`
   margin-top: 1rem;
 `;
 
-const AuthForm = () => {
+const textMap = {
+  login: "로그인",
+  reg: "회원가입",
+};
+
+const AuthForm = ({ type }) => {
+  const text = textMap[type];
   return (
     <StyledAuthForm>
-      <h3>로그인</h3>
+      <h3>{text}</h3>
       <form>
         <StyledInput autoComplete="username" name="username" placeholder="아이디" />
         <StyledInput autoComplete="password" name="password" placeholder="비밀번호" type="password" />
+        {type === "reg" && (
+          <StyledInput autoComplete="new-password" name="passwordConfirm" placeholder="비밀번호 확인" type="password" />
+        )}
         <ButtonWidthMarginTop cyan fullWidth>
-          로그인
+          {text}
         </ButtonWidthMarginTop>
       </form>
-      <Footer>
-        <Link to="/reg">회원가입</Link>
-      </Footer>
+      <Footer>{type === "login" ? <Link to="/reg">회원가입</Link> : <Link to="/login">로그인</Link>}</Footer>
     </StyledAuthForm>
   );
 };
