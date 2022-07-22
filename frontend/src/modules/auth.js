53ef19a697751a6503cb81d7bb0751ea3fd3d6e6
lhk3337 <lhk3337@naver.com>
2022-07-22 15:49:10 +0900

    [FEAT] redux id, pw, 데이터 설정

--- frontend/src/modules/auth.js
+++ frontend/src/modules/auth.js
@@ -1,14 +1,43 @@
 import { createAction, handleActions } from "redux-actions";
+import produce from "immer";
 
-const SAMPLE_ACTION = "auth/SAMPLE_ACTION";
+const CHANGE_FIELD = "auth/CHANGE_FIELD";
+const INITIALIZE_FORM = "auth/INITIALIZE_FORM";
 
-export const sampleAction = createAction(SAMPLE_ACTION);
+export const changeFiled = createAction(CHANGE_FIELD, ({ form, key, value }) => ({
+  form,
+  key,
+  value,
+}));
 
-const initialState = {};
+export const initializeForm = createAction(INITIALIZE_FORM, (form) => form);
+
+const initialState = {
+  reg: {
+    username: "",
+    password: "",
+    passwordConfirm: "",
+  },
+  login: {
+    username: "",
+    password: "",
+  },
+};
 
 const auth = handleActions(
   {
-    [SAMPLE_ACTION]: (state, action) => state,
+    [CHANGE_FIELD]: (state, { payload: { form, key, value } }) =>
+      /*
+      immer 라이브러리 
+      produce(currentState, draft => {function})
+      state : 업데이트 하고자 하는 현재 상태값
+      draft : 현재 상태값을 복사하여 적용
+      produce inner function : 어떻게 업데이트 할지 정의하는 함수
+      */
+      produce(state, (draft) => {
+        draft[form][key] = value;
+      }),
+    [INITIALIZE_FORM]: (state, { payload: form }) => ({ ...state, [form]: initialState[form] }),
   },
   initialState
 );
