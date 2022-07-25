b6156d302427a2907631d682b655163ec5147473
lhk3337 <lhk3337@naver.com>
2022-07-25 12:17:08 +0900

    [FIX] 회원가입 api path 수정

--- frontend/src/lib/api/auth.js
+++ frontend/src/lib/api/auth.js
@@ -2,6 +2,6 @@ import clientApi from "./client";
 export const login = ({ username, password }) => clientApi.post("/api/auth/login", { username, password });
 //post방식이고, /api/auth/login일때 {username, password}의 데이터를 보냄
 
-export const reg = ({ username, password }) => clientApi.post("/api/auth/reg", { username, password });
+export const reg = ({ username, password }) => clientApi.post("/api/auth/register", { username, password });
 
 export const check = () => clientApi.get("/api/auth/check");
