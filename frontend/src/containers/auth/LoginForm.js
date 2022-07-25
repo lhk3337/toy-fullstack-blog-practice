91ec2c3cb01ca02cdfe6fef53e934813f1addfbd
lhk3337 <lhk3337@naver.com>
2022-07-25 16:38:51 +0900

    [FEAT] 로그인 구현하기

--- frontend/src/containers/auth/LoginForm.js
+++ frontend/src/containers/auth/LoginForm.js
@@ -1,13 +1,20 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { changeFiled, initializeForm } from "modules/auth"; //action function
+import { useNavigate } from "react-router-dom";
+
+import { changeFiled, initializeForm, login } from "modules/auth"; //action function
 import AuthForm from "components/auth/AuthForm";
+import { check } from "modules/user";
 
 const LoginForm = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const { form } = useSelector(({ auth }) => ({
+  const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
     form: auth.login,
+    auth: auth.auth,
+    authError: auth.authError,
+    user: user.user,
   })); // redux에서 상태 데이터 가져오기
 
   /* 
@@ -28,11 +35,32 @@ const LoginForm = () => {
 
   const onSubmit = (e) => {
     e.preventDefault();
+    const { username, password } = form;
+    dispatch(login({ username, password }));
   };
 
   useEffect(() => {
     dispatch(initializeForm("login"));
   }, [dispatch]);
+
+  useEffect(() => {
+    if (authError) {
+      console.log("오류");
+      console.log(authError);
+      return;
+    }
+    if (auth) {
+      console.log("회원가입 성공");
+      console.log(auth);
+      dispatch(check());
+    }
+  }, [auth, authError, dispatch]);
+
+  useEffect(() => {
+    if (user) {
+      navigate("/");
+    }
+  }, [navigate, user]);
 
   return <AuthForm type="login" form={form} onChange={onChange} onSubmit={onSubmit} />;
 };
