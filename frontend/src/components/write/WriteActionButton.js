4732bf5303f03dcee3fd57e73d41b0aee1820630
lhk3337 <lhk3337@naver.com>
2022-07-28 14:50:40 +0900

    [FEAT] 포스트, 취소 버튼 component

---
+++ frontend/src/components/write/WriteActionButton.js
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+import Button from "components/common/Button";
+
+const StyledWriteActionButton = styled.div`
+  margin-top: 1rem;
+  margin-bottom: 3rem;
+  /* button + button {
+    margin-left: 0.5rem;
+  } */
+`;
+
+const StyledButton = styled(Button)`
+  height: 2.125rem;
+  // & + & : StyledButton이 바로 옆에 붙어있을 때 <StyledButton /> next to <StyledButton /> 여기서는 취소 버튼의 css에 해당
+  & + & {
+    margin-left: 0.5rem;
+  }
+`;
+
+const WriteActionButton = ({ onCancel, onPublish }) => {
+  return (
+    <StyledWriteActionButton>
+      <StyledButton cyan onClick={onPublish}>
+        포스트 등록
+      </StyledButton>
+      <StyledButton onClick={onCancel}>취소</StyledButton>
+    </StyledWriteActionButton>
+  );
+};
+
+export default WriteActionButton;
--- frontend/src/pages/WritePage.js
+++ frontend/src/pages/WritePage.js
@@ -2,12 +2,14 @@ import React from "react";
 import Editor from "components/write/Editor";
 import Responsive from "components/common/Responsive";
 import TagBox from "components/write/TagBox";
+import WriteActionButton from "components/write/WriteActionButton";
 
 const WritePage = () => {
   return (
     <Responsive>
       <Editor />
       <TagBox />
+      <WriteActionButton />
     </Responsive>
   );
 };
