de38cd9d1884e188928703f8f9486e48fd26b789
lhk3337 <lhk3337@naver.com>
2022-07-31 23:41:44 +0900

    [FEAT] 읽어들일 post API 연동

--- frontend/src/lib/api/posts.js
+++ frontend/src/lib/api/posts.js
@@ -2,3 +2,5 @@ import clientApi from "./client";
 export const writePost = ({ title, body, tags }) => {
   return clientApi.post("/api/posts", { title, body, tags });
 };
+
+export const readPost = (id) => clientApi.get(`/api/posts/${id}`);
