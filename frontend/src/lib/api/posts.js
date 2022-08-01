450deeb01589e7cbf061b68bbfc25c6426f6b8bd
lhk3337 <lhk3337@naver.com>
2022-08-01 17:39:46 +0900

    [FEAT] 포스트 리스트 api 설정하기

--- frontend/src/lib/api/posts.js
+++ frontend/src/lib/api/posts.js
@@ -1,6 +1,12 @@
+import qs from "qs";
 import clientApi from "./client";
 export const writePost = ({ title, body, tags }) => {
   return clientApi.post("/api/posts", { title, body, tags });
 };
 
 export const readPost = (id) => clientApi.get(`/api/posts/${id}`);
+
+export const listPosts = ({ page, username, tag }) => {
+  const queryString = qs.stringify({ page, username, tag });
+  return clientApi.get(`/api/posts?${queryString}`);
+};
